3a1919fb72001a508bfe188ded1ba48a32b60922
Md Saif Ali <md.saif130@gmail.com>
2024-09-12 11:18:39 +0530

    Initial commit

---
+++ src/assets/services_data.js
@@ -0,0 +1,35 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Web Application Development",
+        s_desc:"Web development is the process of building, programming..."
+        
+    },
+    {
+        s_no:"02",
+        s_name:"UI/UX Design Implementation",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"03",
+        s_name:" Frontend Optimization",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"04",
+        s_name:"Testing and Quality Assurance",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"05",
+        s_name:"Code Review and Auditing",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"06",
+        s_name:"Maintenance and Support",
+        s_desc:"Web development is the process of building, programming..."
+    },
+]
+ 
+export default Services_Data;
